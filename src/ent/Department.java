049334facdba84799f43f4fec7ffe93e6c8b08f9
Patrick Pericles <patrickvieira.pericles@gmail.com>
2020-03-13 15:28:40 -0300

    Department Class Created

---
+++ src/ent/Department.java
@@ -0,0 +1,70 @@
+package ent;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer ID;
+	private String Name;
+	
+	public Department() {
+	
+	}
+
+	public Department(Integer iD, String name) {
+		super();
+		ID = iD;
+		Name = name;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department ID: " + ID + ", Name: " + Name ;
+	}
+
+}
+	
+
