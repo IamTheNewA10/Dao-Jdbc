9592e8b27127a128240f0f73854d1e8771b042b4
Patrick Pericles <patrickvieira.pericles@gmail.com>
2020-03-16 12:05:41 -0300

    Creating SellerDaoJDBC and DaoFactory

--- src/dao/DepartmentDao.java
+++ src/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import ent.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deletedById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
--- src/dao/SellerDao.java
+++ src/dao/SellerDao.java
@@ -1,5 +1,15 @@
 package dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import ent.Department;
+import ent.Seller;
+
+    public interface SellerDao {
+    	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deletedById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 }
